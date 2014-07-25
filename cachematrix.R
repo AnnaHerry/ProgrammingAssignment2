a78554d76ccb7ae4e86c1938087870851b2ca88c
AnnaHerry <ania.herry@gmail.com>
2014-07-26 00:26:38 +0200

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,19 @@
 ## The "makeCacheMatrix" function creates and caches variable
 ## named "inverse".This variable serves to store the value of 
 ## inverse matrix of a given square matrix "x".
-## Also this function creates a list of internal functions 
+## Also this function creates a list of internal
+## functions "set", "get", "getinv", "setinv".
 
 
-makeCacheMatrix <- function(x = matrix()) {
-  ## Initialise variable "inverse", the first value is set to NULL
-  inverse <- NULL 
- 
-  ## Store the value and labelle the adress as "inverse"
-  set <- function(y)  
-  { x <<- y
+makeCacheMatrix <- function(x= matrix ()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
     inverse <<- NULL
   }
-  
-  ## Return original matrix
-  get <- function() x 
-  
-  ## Store the inverse matrix
-  setinv <- function(solve) inverse <<- solve 
-  
-  ## Return inverse matrix
-  getinv <- function() inverse 
-  
-  ## Create a list on internal functions "set", "get", "setinv", "getinv"
+  get <- function() x
+  setinv <- function(solve) inverse <<- solve
+  getinv <- function() inverse
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -31,34 +21,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## By using the output from "makeCacheMatrix" function 
-## the "cacheSolve" function computes the inverse matrix, stores 
-## and returns it. In the case if inverse matrix has already been 
-##  computed and cashed, the "cacheSolve" retrieves and returns it.
+## the following  function "cacheSolve"  computes the inverse matrix, 
+## stores and returns it. In the case if inverse matrix has already been 
+## computed and cashed, the "cacheSolve" retrieves and returns it.
 
 cacheSolve <- function(x, ...) {
-     ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinv()
-  
-  ## Check if it is available 
   if(!is.null(inverse)) {
-    
-   ## if TRUE then write message 
     message("getting cached data")
-    
-    ## and return it
     return(m)
   }
-  
-  ## if FALSE then get the original matrix
   data <- x$get()
-  
-  ## Compute the inverse
   inverse <- solve(data, ...)
-  
-  ## Store it
   x$setinv(inverse)
-  
-  ## Return it
   inverse
-
 }
